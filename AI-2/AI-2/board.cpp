8022c2ddaf61f6a86eec7bae54c53671a0b33551
Nicholas Kaiser <nkaiser@mines.edu>
2020-02-24 14:02:15 -0700

    filled in definition for board

--- AI-2/AI-2/board.cpp
+++ AI-2/AI-2/board.cpp
@@ -4,7 +4,16 @@ board::board()
 {
 }
 
-bool board::addPiece(int column)
+bool board::addPiece(int column, string color)
 {
+	//column is indexed at one for user convenience, so minus one for use in array
+	column--;
+
+	for (int i = 0; i < rows; i++) {
+		if (pieces[column][i].color == "") {
+			pieces[column][i].color = color;
+			return true;
+		}
+	}
 	return false;
 }
